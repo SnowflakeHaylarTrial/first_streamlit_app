9ab94172c0b9705417174aceee0b8a198c99db0f
SnowflakeHaylarTrial <125705495+SnowflakeHaylarTrial@users.noreply.github.com>
2023-02-17 15:12:07 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,6 @@ streamlit.header("Breakfast Favorites")
 streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
 streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
 streamlit.text("🐔 Hard-Boiled Free_Range Egg")
-streamlit.text("🥑🍞Avacado Toast")
+streamlit.text("🥑🍞 Avacado Toast")
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
