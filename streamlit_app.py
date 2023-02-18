f2d70eb4f79629b1a64b001dc1f23c33318e6a7d
SnowflakeHaylarTrial <125705495+SnowflakeHaylarTrial@users.noreply.github.com>
2023-02-18 02:20:13 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,8 +36,9 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # take the data from the variable and pump it into a table
 streamlit.dataframe(fruityvice_normalized)
-# don't run anything past here while we troubleshoot
 
+# don't run anything past here while we troubleshoot
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
